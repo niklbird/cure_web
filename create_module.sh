c5ee5b924b93f88471c42a88dcc812d64ca16488
tkh42 <tobki4242@gmail.com>
2025-02-13 17:02:44 +0100

    Move package to frontend folder

--- create_module.sh
+++ create_module.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cargo build --target wasm32-unknown-unknown --release
-wasm-bindgen target/wasm32-unknown-unknown/release/cure_web.wasm --out-dir ./pkg --target web
+wasm-bindgen target/wasm32-unknown-unknown/release/cure_web.wasm --out-dir ./src/frontend/pkg --target web
