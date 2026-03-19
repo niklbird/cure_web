a9cc3bffc28b0630aeee58bcb193aada0c0b0e29
Tobias Kirsch <tkirsch@em.uni-franfurt.de>
2026-03-19 18:16:26 +0100

    Re-enable linting

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: './',
+  lintOnSave: true,
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
